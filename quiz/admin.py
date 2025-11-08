6b8ef942af783a88099df9787d708221ce7f4a6a
SELORM <selormmatanawui2006@gmail.com>
2025-11-08 12:47:23 +0000

    adding the questions model

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.contrib import admin
+from .models import Question  # import your model
+
+admin.site.register(Question)  # register it so it shows in admin
 
 # Register your models here.
--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    option_a = models.CharField(max_length=200)
+    option_b = models.CharField(max_length=200)
+    option_c = models.CharField(max_length=200)
+    option_d = models.CharField(max_length=200)
+    correct_option = models.CharField(
+        max_length=1,
+        choices=[('A', 'A'), ('B', 'B'), ('C', 'C'), ('D', 'D')],
+        help_text="Choose the correct option"
+    )
+    def __str__(self):
+        return self.question_text
